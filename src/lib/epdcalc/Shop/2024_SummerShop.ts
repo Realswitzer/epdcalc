db0e0bde592141cfba428b97fb03cb5be9e5f8d1
Realswitzer <switzr@tuta.io>
2024-10-10 17:13:03 -0500

    get ready to make some awful pr
    Signed-off-by: Realswitzer <switzr@tuta.io>

---
+++ src/lib/epdcalc/Shop/2024_SummerShop.ts
@@ -0,0 +1,12 @@
+import type { ShopCategory } from '../types';
+export const elusive: ShopCategory = {
+	lemonade_stand: 2_500,
+	scorcher: 5_000
+};
+export const illustrious: ShopCategory = {
+	shrine_of_tartarus: 19_999,
+	strawberry_lemonade_stand: 12_499,
+	sunshadow_operative: 19_999,
+	summer_stryker: 29_999,
+	visible_man: 22_222
+};
