cd658197124e9365049a5872053c59e539a5d284
Realswitzer <switzr@tuta.io>
2026-02-03 11:16:46 -0600

    start scaffolding enemy instance

---
+++ src/instance/enemy.ts
@@ -0,0 +1,13 @@
+import { Enemy } from "../types";
+
+export interface Debuffs {
+  rad?: number;
+  scorch?: number;
+  frostbite?: number;
+  shred?: number;
+  stun?: number;
+  bleed?: number;
+  burn?: number;
+  rupture?: number;
+  // first_strike, implement lengths
+}
