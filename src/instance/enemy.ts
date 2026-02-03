114fb92707887fa0fbe49d374743a785d9bee9eb
Realswitzer <switzr@tuta.io>
2026-02-03 11:49:09 -0600

    enemy scaffold!

--- src/instance/enemy.ts
+++ src/instance/enemy.ts
@@ -1,4 +1,4 @@
-import { Enemy } from "../types";
+import { Enemy as _Enemy } from "../types";
 
 export interface Debuffs {
   rad?: number;
@@ -9,5 +9,59 @@ export interface Debuffs {
   bleed?: number;
   burn?: number;
   rupture?: number;
-  // first_strike, implement lengths
+  // first_strike, implement lengths. possibly via setTimeout?
+}
+
+export class Enemy implements _Enemy {
+  public id!: string;
+  public name!: string;
+  public health!: number;
+  public maxHealth: number;
+  public shield!: number;
+  public boss?: boolean;
+  public armor?: boolean;
+  public corrupted?: boolean;
+
+  public debuffs: Required<Debuffs> = {
+    rad: 0,
+    scorch: 0,
+    frostbite: 0,
+    shred: 0,
+    stun: 0,
+    bleed: 0,
+    burn: 0,
+    rupture: 0,
+  };
+
+  public constructor(enemy: _Enemy) {
+    Object.assign(this, enemy);
+    this.maxHealth = enemy.health;
+  }
+
+  public setDebuff(debuff: { type: keyof Debuffs; strength: number }) {
+    this.debuffs[debuff.type] += debuff.strength;
+  }
+
+  public setTimedDebuff(debuff: {
+    type: keyof Debuffs;
+    strength: number;
+    duration: number;
+  }) {
+    this.debuffs[debuff.type] += debuff.strength;
+    setTimeout(() => {
+      this.debuffs[debuff.type] -= debuff.strength;
+    }, debuff.duration * 1000);
+  }
+
+  public getRadMultiplier(): number {
+    return this.debuffs.rad + 1;
+  }
+
+  public getScorchMultiplier(): number {
+    return this.debuffs.scorch + 1;
+  }
+
+  public getRuptureMultiplier(): number {
+    return this.debuffs.rupture + 1;
+  }
 }
