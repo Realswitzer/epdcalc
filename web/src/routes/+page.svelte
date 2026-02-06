8c39bb08faa22a375d4c577a401ddad6cad50934
Realswitzer <switzr@tuta.io>
2026-02-06 12:31:56 -0600

    a

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,7 +2,14 @@
   import { towers } from "epdcalc";
   import { TowerInstance } from "epdcalc/instance";
   import { damagePerAction } from "epdcalc/formulas";
+  import type { Tower } from "epdcalc/types";
 
   let selected = towers.elder_godfather;
   let upgrade = selected.paths[1];
 </script>
+
+<select id="towers">
+  {#each Object.keys(towers) as tower}
+    <option id={tower}>{tower}</option>
+  {/each}
+</select>
