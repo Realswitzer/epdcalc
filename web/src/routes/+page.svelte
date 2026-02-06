38297e25665a292498fc9610f165cf45d03c9674
Realswitzer <switzr@tuta.io>
2026-02-06 13:01:07 -0600

    how do i use svelte

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<script lang="ts">
-  import { towers } from "epdcalc";
-  import { TowerInstance } from "epdcalc/instance";
-  import { damagePerAction } from "epdcalc/formulas";
-  import type { Tower } from "epdcalc/types";
+<main>
+  <p>this page intentionally left blank</p>
+</main>
 
-  let selected = towers.elder_godfather;
-  let upgrade = selected.paths[1];
-</script>
-
-<select id="towers">
-  {#each Object.keys(towers) as tower}
-    <option id={tower}>{tower}</option>
-  {/each}
-</select>
+<style>
+  main {
+    display: none;
+  }
+</style>
---
+++ web/src/routes/calculator/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { towers } from "epdcalc";
+  import { TowerInstance } from "epdcalc/instance";
+  import { damagePerAction } from "epdcalc/formulas";
+  import type { Tower } from "epdcalc/types";
+
+  let selected = towers.elder_godfather;
+  let upgrade = selected.paths[1];
+</script>
+
+<select id="towers">
+  {#each Object.keys(towers) as tower}
+    <option id={tower}>{tower}</option>
+  {/each}
+</select>
