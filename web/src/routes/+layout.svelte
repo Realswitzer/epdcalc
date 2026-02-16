2791ec21e549b6cde18643d4611a0d0b7ce21ae1
Sylvyon <switzr@tuta.io>
2026-02-16 00:45:26 -0600

    fix compiling
    Signed-off-by: Sylvyon <switzr@tuta.io>

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -3,4 +3,18 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+  <meta name="og:title" content="epdcalc | sylvyon.dev" />
+  <meta
+    name="og:description"
+    content="poorly made calculator for entry point defense"
+  />
+  <meta name="og:type" content="website"/>
+  <meta name="keywords" content="epdcalc, sylvyon.dev" />
+  <meta name="robots" content="noindex, nofollow" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="language" content="English" />
+  <meta name="author" content="sylvyon" />
+</svelte:head>
+
 {@render children()}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  server: {
+    allowedHosts: [
+      "vite-internal-9478cbb7-b2b0-42ac-8498-3484b1b5d7b5.sylvyon.dev",
+    ],
+  },
 });
